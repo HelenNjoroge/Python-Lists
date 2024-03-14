3103651a351232fed8bdcde1b330fa8c51d499b1
HelenWairimu <helenanjoro@gmail.com>
2024-03-14 16:11:51 +0300

    my first commit

---
+++ Lists.py
@@ -0,0 +1,26 @@
+mylist = []
+print(mylist)
+mylist.append(10)
+
+mylist.append(20)
+
+mylist.append(30)
+
+mylist.append(40)
+print(mylist)
+
+mylist[1]=15
+print(mylist)
+
+mylist.extend([50,60,70])
+print(mylist)
+
+mylist.remove(70)
+print(mylist)
+
+mylist.sort()
+print(mylist)
+
+print(mylist.index(30))
+
+
